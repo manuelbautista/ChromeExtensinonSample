f85f79198e2934f0f950ae2e777296c252a3c6c2
Manuel Bautista <b0hcoder@gmail.com>
2020-05-13 14:38:48 -0400

    changing alert message

--- content.js
+++ content.js
@@ -44,10 +44,10 @@ chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
       if( request.message === "start" ) {
        start();
-           }
+      }
     }
   );
 
   function start(){
-      alert("started");
+      alert("started!!!");
   }
